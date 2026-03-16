ffbac1204f90f6e23f97ae344dca5c333d17d730
KhaosKoder <KhaosKoder@gmail.com>
2026-03-16 08:43:16 +0200

    chore: track bin/ scripts in git, keep compiled bin/ ignored

---
+++ bin/build-test.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+param(
+    [string]$Configuration = 'Debug'
+)
+
+Push-Location (Resolve-Path "$PSScriptRoot\..")
+try {
+    dotnet build KoreForge.Time.sln --force -c $Configuration
+    dotnet test  KoreForge.Time.sln -c $Configuration --no-build `
+        --logger "html;LogFileName=TestResults.html" `
+        --results-directory out/TestResults
+    Write-Host 'Test results: out/TestResults/TestResults.html' -ForegroundColor Green
+} finally {
+    Pop-Location
+}
